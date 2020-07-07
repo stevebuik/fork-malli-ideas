d50c47d35ba8e4d6c6c4748f2c548d22608ced82
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2020-07-07 11:52:52 +1000

    added “switching-records” card
    showing re-loading of Fork forms not working

--- src/basics.cljs
+++ src/basics.cljs
@@ -6,49 +6,79 @@
             [core]
             [fork.re-frame :as fork]))
 
-(defonce app-db (r/atom {:record1 {"id"       "1234"
+(defonce app-db (r/atom {:editing :record1
+                         :record1 {"id"       "1234"
                                    "name"     "Lucio"
                                    "location" "London"}
                          :record2 {"id"       "5678"
                                    "name"     "Tommi"
                                    "location" "Tampere"}}))
 
-;;; READ / DISPLAY
+(def form-config {:header      core/form-header
+                  :form-class  ""
+                  :inner-class ""
+                  :input-rows  [{:inputs [{:type         "text"
+                                           :field-name   "name"
+                                           :place-holder "Enter name"
+                                           :label        "Name"}]
+                                 :class  ""}
+                                {:inputs [{:type         "text"
+                                           :field-name   "location"
+                                           :place-holder "Enter location"
+                                           :label        "Location"}]
+                                 :class  ""}]})
 
-(defn- display
-  [re-frame-sub _]
-  [:div {:style {:margin-bottom 30}}
+(defcard-rg simple-validations
+            (fn [re-frame-sub _]
+              [:div {:style {:margin-bottom 30}}
 
-   [:p "Basic form: both fields have validation. try clearing the name or changing the location"]
+               [:p "Basic form: both fields have validation. try clearing the name or changing the location"]
 
-   [:div {:style {:border  "1px solid lightgrey"
-                  :padding 20}}
-    [fork/form {:initial-values (:record1 @re-frame-sub)
-                :validation     (core/validator-for-humans [:map
-                                                            ["name" [:string {:min 2 :max 50}]]
-                                                            ["location" [:enum {:error/message "Must be London or Tampere"}
-                                                                         "London" "Tampere"]]])}
-     (core/multi-row-form {:header      core/form-header
-                           :form-class  ""
-                           :inner-class ""
-                           :input-rows  [{:inputs [{:type         "text"
-                                                    :field-name   "name"
-                                                    :place-holder "Enter name"
-                                                    :label        "Name"}]
-                                          :class  ""}
-                                         {:inputs [{:type         "text"
-                                                    :field-name   "location"
-                                                    :place-holder "Enter location"
-                                                    :label        "Location"}]
-                                          :class  ""}]})]]
+               [:div {:style {:border  "1px solid lightgrey"
+                              :padding 20}}
+                [fork/form {:initial-values (:record1 @re-frame-sub)
+                            :validation     (core/validator-for-humans [:map
+                                                                        ["name" [:string {:min 2 :max 50}]]
+                                                                        ["location" [:enum {:error/message "Must be London or Tampere"}
+                                                                                     "London" "Tampere"]]])}
+                 (core/multi-row-form form-config)]]
 
-   [:p "Questions:"
-    [:ol
-     [:li "can the validation fn be pre-compiled and still used with m/explain"]]]
-   ])
+               [:p "Questions:"]
+               [:ol
+                [:li "can the validation fn be pre-compiled and still used with m/explain"]]
+               ])
+            app-db
+            {:inspect-data false})
 
-(defcard-rg form-hello
-            display
+(defcard-rg switching-records
+            (fn [re-frame-sub _]
+              [:div {:style {:margin-bottom 30}}
+
+               [:p "Demo of loading different maps into a form. Did not work in early impls."]
+
+               [:p (str "Editing: " (get-in @re-frame-sub [(:editing @re-frame-sub) "name"]))]
+
+               [:div {:style {:margin-bottom "1rem"}}
+                [:button {:onClick (fn [_]
+                                     (swap! app-db assoc :editing :record2))}
+                 "Load Tommi"]
+                [:button {:onClick (fn [_]
+                                     (swap! app-db assoc :editing :record1))}
+                 "Load Lucio"]]
+
+               [:div {:style {:border  "1px solid lightgrey"
+                              :padding 20}}
+                [fork/form {:initial-values (get @re-frame-sub (:editing @re-frame-sub))
+                            :validation     (core/validator-for-humans [:map
+                                                                        ["name" [:string {:min 2 :max 50}]]
+                                                                        ["location" [:enum {:error/message "Must be London or Tampere"}
+                                                                                     "London" "Tampere"]]])}
+                 (core/multi-row-form form-config)]]
+
+               [:p "Questions:"]
+               [:ol
+                [:li "why does Tommi not load in the form?"]]
+               ])
             app-db
             {:inspect-data true})
 
