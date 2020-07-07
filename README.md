210ac4eb871e992e34556df4959cf665dc55b36f
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2020-07-07 11:01:49 +1000

    initial attempt at github pages devcards

---
+++ README.md
@@ -0,0 +1,12 @@
+# Using Fork and Malli in Re-Frame
+
+## Local Development
+
+- clone this repo
+- start a REPL using deps.edn
+- eval the 4 forms in user.clj
+- open the devcards in your browser
+
+TODO 
+
+How to build/deploy devcards on Github pages
--- dev/user.clj
+++ dev/user.clj
@@ -3,12 +3,16 @@
 
 (comment
 
+  ; start the local server
   (require '[shadow.cljs.devtools.server :as server])
   (server/start!)
   (require '[shadow.cljs.devtools.api :as shadow])
   (shadow/watch :devcards)
 
+  ; build for github pages
+  (shadow/release :devcards)
 
+  ; stop the local watch
   (shadow/stop-worker :devcards)
 
   )
