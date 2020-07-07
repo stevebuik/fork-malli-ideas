d4773bc55ddeb4c2039fababa9384bb0a599f071
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2020-07-07 18:54:12 +1000

    fixed release build for github pages demo
    thanks Thomas Heller!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Using Fork and Malli in Re-Frame
 
-Live (broken) demo at https://stevebuik.github.io/fork-malli-ideas
+Live demo at https://stevebuik.github.io/fork-malli-ideas
 
 ## Local Development
 
@@ -9,6 +9,3 @@ Live (broken) demo at https://stevebuik.github.io/fork-malli-ideas
 - eval the 4 forms in user.clj
 - open the devcards in your browser
 
-TODO 
-
-How to build/deploy devcards on Github pages
--- src/devcards.cljs
+++ src/devcards.cljs
@@ -1,13 +1,10 @@
 (ns devcards
   (:require [cljs.pprint :refer [pprint]]
-            [devcards.util.markdown]
-            [devcards.core :refer [start-devcard-ui!]
-             :refer-macros [defcard defcard-doc deftest dom-node reagent]]
+            [devcards.core :refer [start-devcard-ui!]]
             [readme]
             [basics]
             [bulma]))
 
 (enable-console-print!)
 
-(defn ^:export init []
-  (start-devcard-ui!))
+(defn ^:export init [] (start-devcard-ui!))
